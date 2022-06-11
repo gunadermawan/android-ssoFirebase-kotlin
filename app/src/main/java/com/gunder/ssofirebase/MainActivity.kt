61878a1f5937185d823422f407ec6c86a3b3a309
gunadermawan <gunadermawan62@gmail.com>
2022-06-11 23:18:36 +0700

    update system: add json file config for firebase

--- app/src/main/java/com/gunder/ssofirebase/MainActivity.kt
+++ app/src/main/java/com/gunder/ssofirebase/MainActivity.kt
@@ -2,11 +2,20 @@ package com.gunder.ssofirebase
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.gunder.ssofirebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
         supportActionBar?.hide()
+        requestLogin()
+    }
+
+    private fun requestLogin() {
     }
 }
